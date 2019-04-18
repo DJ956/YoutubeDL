985666140bd1eac966b6a94db67e8f15da9f8607
DJ956 <dexter13576@gmail.com>
2019-04-18 22:44:10 +0900

    first commit
    最低限の機能を実装

---
+++ YoutubeDL/YoutubeDL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary;
+using System.IO;
+
+namespace YoutubeDL
+{
+    class Program
+    {
+        static string ROOT = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("URL:");
+            var url = Console.ReadLine();
+
+            var youtube = YouTube.Default;
+
+            var video = youtube.GetVideo(url);
+
+            var fileName = $"{video.Title}.{video.FileExtension}";
+            var path = Path.Combine(ROOT, fileName);
+
+            Console.WriteLine($"Download:{video.Title}");
+            Console.WriteLine($"Save to:{path}");
+
+            File.WriteAllBytes(path, video.GetBytes());
+
+            Console.WriteLine("Done");
+        }
+    }
+}
